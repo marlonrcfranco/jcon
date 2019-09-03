990c3d3a09ce7fff2c14852a34dcdc37dee15317
Marlon Rubio de Carvalho Franco <marlon.franco@adapcon.local>
2019-09-03 11:59:36 -0300

    Initial Main for executing stand-alone

--- src/main/java/com/marlonrcfranco/Main.java
+++ src/main/java/com/marlonrcfranco/Main.java
@@ -1,31 +1,49 @@
 package com.marlonrcfranco;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        /**
-         *  Conectores
-         *  ╔═══════════════╦═══════════════════════════════════════╦════════════════════════════════════╗
-         *  ║ Type		    ║ Descrição							    ║ Parâmetros necessários			 ║
-         *  ╠═══════════════╩═══════════════════════════════════════╩════════════════════════════════════╣
-         *  ║ filesystem	│ Acesso a arquivos na própria 			│ basePath							 ║
-         *  ║				│ máquina, sem a necessidade de IP.		│									 ║
-         *  ║───────────────┼───────────────────────────────────────┼────────────────────────────────────║
-         *  ║ smb1			│ Acesso a arquivos remotos utilizando	│ basePath,ip,username,password		 ║
-         *  ║				│ o protocolo SAMBA 1 					│									 ║
-         *  ║				│ até Windows 10).						│									 ║
-         *  ║───────────────┼───────────────────────────────────────┼────────────────────────────────────║
-         *  ║ smb23		    │ Acesso a arquivos remotos utilziando	│ basePath,ip,username,password		 ║
-         *  ║				│ o protocolo SAMBA2 ou SAMBA3			│									 ║
-         *  ║				│ (após Windows 10).					│									 ║
-         *  ║───────────────┼───────────────────────────────────────┼────────────────────────────────────║
-         *  ║ nfs			│ Acesso a arquivos remotos utilizando 	│ basePath,ip,username,password		 ║
-         *  ║				│ o protocolo NFS (Linux, Unix based OS)│									 ║
-         *  ╚════════════════════════════════════════════════════════════════════════════════════════════╝
-         *
-         */
-
+        String input="";
+        String info = "\n" +
+                "\n═════════════════════════════════════════════════\n" +
+                "     ╔═╗       ╔═╗  ╔╗\n" +
+                "     ║ ╠═══╦═══╣ ╚╗ ║║\n" +
+                " ╔═╗ ║ ║ ╔═╣╔═╗║╔╗╚╗║║\n" +
+                " ║ ╚═╝ ║ ╚═╣╚═╝║║╚╗╚╝║\n" +
+                " ╚═════╩═══╩═══╩╝ ╚══╝ v0.1\n" +
+                "\n@marlonrcfranco" +
+                "\nhttps://github.com/marlonrcfranco/jcon" +
+                "\n\nBased on the projects:" +
+                "\n  jCIFS: https://www.jcifs.org/ (for SMB1)" +
+                "\n  smbj : https://github.com/hierynomus/smbj (for SMB2/3)" +
+                "\n═════════════════════════════════════════════════";
+        String connectors =
+        "*  Connectors\n" +
+                " ╔═══════════════╦═══════════════════════════════════════╦════════════════════════════════════╗\n" +
+                " ║ type          ║ Description                           ║ Parameters needed                  ║\n" +
+                " ╠═══════════════╩═══════════════════════════════════════╩════════════════════════════════════╣\n" +
+                " ║ filesystem    │ Access the local filesystem without   │ basePath                           ║\n" +
+                " ║               │ informing the IP address.             │                                    ║\n" +
+                " ║───────────────┼───────────────────────────────────────┼────────────────────────────────────║\n" +
+                " ║ smb1          │ Acesso a arquivos remotos utilizando  │ basePath,ip,username,password      ║\n" +
+                " ║               │ o protocolo SMB 1                     │                                    ║\n" +
+                " ║               │ até Windows 10).                      │                                    ║\n" +
+                " ║───────────────┼───────────────────────────────────────┼────────────────────────────────────║\n" +
+                " ║ smb23         │ Acesso a arquivos remotos utilziando  │ basePath,ip,username,password      ║\n" +
+                " ║               │ o protocolo SMB 2 ou SMB 3            │                                    ║\n" +
+                " ║               │ (após Windows 10).                    │                                    ║\n" +
+                " ║───────────────┼───────────────────────────────────────┼────────────────────────────────────║\n" +
+                " ║ nfs           │ Acesso a arquivos remotos utilizando  │ basePath,ip,username,password      ║\n" +
+                " ║               │ o protocolo NFS (Linux, Unix based OS)│                                    ║\n" +
+                " ╚════════════════════════════════════════════════════════════════════════════════════════════╝\n" +
+                " \n";
+        String help ="\n" +
+                "\n help [h]         - Show this help" +
+                "\n info [i]         - Show initial info" +
+                "\n connectors [con] - Show info about different connectors" +
+                "\n ";
         /**
          * Compartilhamento
          * \\192.168.35.17
@@ -33,29 +51,28 @@ public class Main {
          * pass: 1nfr4#2017
          */
 
-//        String IP="192.168.35.17";
-        String IP="CSW";
-        String user="Adapcon";
-        String pass="1nfr4#2017";
+        System.out.println(info);
+        while(!"exit,quit,halt,bye".contains(input) || "".equalsIgnoreCase(input)) {
+            System.out.println("\n Type \"help\" for Help.");
+            System.out.print("> ");
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            switch (input) {
+                case "h":
+                case "help":
+                    System.out.println(help);
+                    break;
+                case "i":
+                case "info":
+                case "about":
+                    System.out.println(info);
+                case "con":
+                case "connectors":
+                    System.out.println(connectors);
+                default:break;
+            }
+        }
 
-        JconSMB1 smb = new JconSMB1();
-
-        String response="";
-
-        //response = smb.write("/123/Teste777.txt", "Texto teste 1237777");
-        //response = smb.read("123/Teste777.txt");
-
-        /**
-         * Leitura de arquivo remoto sem autenticação
-         */
-        //response = smb.read("/dados/exportQuery.csv","","");
-
-        /**
-         * Leitura de arquivo local sem autenticação
-         */
-        //response = smb.read("localhost/Users/marlon.franco/Documents/teste.xml","","");
-
-        System.out.println(response);
 
     }
 
